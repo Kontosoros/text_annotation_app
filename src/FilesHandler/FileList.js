7a95b750c271bce1a79f64005c9b6d16833a2751
Michael Kontosoros <mkontosoros@thinkcompass.onmicrosoft.com>
2023-11-15 13:19:40 +0200

    make stable the containers

--- src/FilesHandler/FileList.js
+++ src/FilesHandler/FileList.js
@@ -15,31 +15,29 @@ const FileList = props => {
   props.selectedFile(selectedFile);
 
   return (
-    <div className="file-list">
-      <div className="file-list-scroll">
-        <ul>
-          {props.files.map((file, index) => (
-            <li
-              className={`file-name ${
-                props.selectedFiles.includes(file) ? "selected-file" : ""
-              } ${selectedFile === file ? "clicked-file" : ""}`}
-              key={index}
-              onClick={() => handleFileClick(file)}
-            >
-              <button
-                checked={props.selectedFiles.includes(file)}
-                onChange={() => props.onFileSelect(file)}
+    <div className="file-list-container">
+      <div className=" file-list">
+        <div className="file-list-scroll">
+          <ul>
+            {props.files.map((file, index) => (
+              <li
+                className={`file-name ${
+                  props.selectedFiles.includes(file) ? "selected-file" : ""
+                } ${selectedFile === file ? "clicked-file" : ""}`}
+                key={index}
+                onClick={() => handleFileClick(file)}
               >
-                {file.name}
-              </button>
-            </li>
-          ))}
-        </ul>
+                <button
+                  checked={props.selectedFiles.includes(file)}
+                  onChange={() => props.onFileSelect(file)}
+                >
+                  {file.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-
-      <button className="button-remove" onClick={props.onRemoveFiles}>
-        <label className="label-button-remove">Close Files</label>
-      </button>
     </div>
   );
 };
