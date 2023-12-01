92da157bb8c2b7808e6cb007e73a4a36ff5ac9af
Michael Kontosoros <mkontosoros@thinkcompass.onmicrosoft.com>
2023-12-01 10:24:22 +0200

    add extra text for overlaps

--- src/TextArea/ErrorWindows/OverlapsErrorWindow.js
+++ src/TextArea/ErrorWindows/OverlapsErrorWindow.js
@@ -10,7 +10,7 @@ const OverlapsErrorWindow = ({ resetGoldenData }) => {
         <p>
           Oops! It seems that there are too many overlapping entities. After
           pressing the button, the annotations for the selected message will be
-          removed.
+          removed. Multiple overlaps are allowed only with the label "SEGMENTS_IN."
         </p>
         <button onClick={handleErrorClick}>OK</button>
       </div>
